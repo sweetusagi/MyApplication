795cd41c9c1f942ec94bf72335f14ec67cc8a8c5
Miku <haruharuharuharu234@gmail.com>
2015-04-22 20:21:14 +0900

    make a change.

--- app/src/main/java/com/example/roromiyakaruta/myapplication/MainActivity.java
+++ app/src/main/java/com/example/roromiyakaruta/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
